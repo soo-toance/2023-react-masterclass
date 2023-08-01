83773456efd802f8cbed9caf326b51a130a7aa01
may <soo.toance@gmail.com>
2023-08-01 18:07:48 +0900

    typeScript적용 - onSubmit, onChange 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 import Circle from "./Circle";
 
 function App() {
+  const [value, setValue] = useState("")
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    // console.log(event.currentTarget.value);
+    const { 
+      currentTarget: { value } 
+    } = event; 
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Hello", value);
+  };
+
   return (
     <div>
-      <Circle text="i'm here" borderColor="yello" bgColor="teal"/>
-      <Circle bgColor="tomato"/>
+      <form onSubmit={onSubmit}>
+        <input value={value} onChange={onChange} type="text" placeholder="username" />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
