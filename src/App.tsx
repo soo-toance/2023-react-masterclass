92a1b626f1863536e19654127fe4342ceb457746
may <soo.toance@gmail.com>
2023-08-01 18:23:14 +0900

    typescript적용 - theme , styled.d.ts
    - https://styled-components.com/docs/api#typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,19 @@
 import React, { useState } from "react"
 import Circle from "./Circle";
+import styled from "styled-components"
 
 function App() {
-  const [value, setValue] = useState("")
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    // console.log(event.currentTarget.value);
-    const { 
-      currentTarget: { value } 
-    } = event; 
-    setValue(value);
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("Hello", value);
-  };
-
+  const Container = styled.div`
+    background-color: ${(props) => props.theme.bgColor};  
+  `;
+  const H1 = styled.h1`
+    color: ${(props) => props.theme.textColor};  
+  `;
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input value={value} onChange={onChange} type="text" placeholder="username" />
-        <button>Log in</button>
-      </form>
+      <Container>
+        <H1>PROTECTED</H1>
+      </Container>
     </div>
   );
 }
