52a1f7d841c1f08c2a038e1bffb959a280cf505d
may <soo.toance@gmail.com>
2023-08-03 21:09:07 +0900

    createGlobalStyle 통해 body에 전체 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React, { useState } from "react"
 import Circle from "./Circle";
-import styled from "styled-components"
+import styled, { createGlobalStyle } from "styled-components"
 import Router from "./Router";
-function App() {
-  return <Router>
 
-  </Router>
+const GlobalStyle = createGlobalStyle`
+  body {
+    color: red;
+  }
+`;
+
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+      </Router>
+    </>
+  );
 }
 
 export default App;
