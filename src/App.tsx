0878365e26d543952ad9cefb6fcbbfd97d0cd68b
may <soo.toance@gmail.com>
2023-08-03 21:02:55 +0900

    React router 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import React, { useState } from "react"
 import Circle from "./Circle";
 import styled from "styled-components"
-
+import Router from "./Router";
 function App() {
-  const Container = styled.div`
-    background-color: ${(props) => props.theme.bgColor};  
-  `;
-  const H1 = styled.h1`
-    color: ${(props) => props.theme.textColor};  
-  `;
-  return (
-    <div>
-      <Container>
-        <H1>PROTECTED</H1>
-      </Container>
-    </div>
-  );
+  return <Router>
+
+  </Router>
 }
 
 export default App;
