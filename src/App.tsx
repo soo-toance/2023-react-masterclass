4538f8fb8e512be5214a526160151aee80bd5031
may <soo.toance@gmail.com>
2023-08-04 15:50:50 +0900

    #5.10 React Query part Two - ReactQueryDevTools 연동

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import Circle from "./Circle";
 import styled, { createGlobalStyle } from "styled-components"
 import Router from "./Router";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -73,6 +74,7 @@ function App() {
     <>
       <GlobalStyle />
       <Router />
+      <ReactQueryDevtools initialIsOpen={true} />
     </>
   );
 }
