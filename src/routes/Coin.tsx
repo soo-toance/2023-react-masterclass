e0257ae1fd7013023220ac1979bf37bc9d1dc4e1
may <soo.toance@gmail.com>
2023-08-04 16:38:05 +0900

    #5.15 Final Touches - 코인 상세 페이지 내 가격 연동

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -191,8 +191,8 @@ function Coin() {
                   <span>${infoData?.symbol}</span>
                 </OverviewItem>
                 <OverviewItem>
-                  <span>Open Source:</span>
-                  <span>{infoData?.open_source ? "Yes" : "No"}</span>
+                 <span>Price:</span>
+                 <span>${tickersData?.quotes.USD.price.toFixed(3)}</span>
                 </OverviewItem>
               </Overview>
 
