e8267a454fe84dbb1d06ff7361d0d9a83761dc4f
may <soo.toance@gmail.com>
2023-08-04 15:43:59 +0900

    #5.9 React Query part One - react query 이용해 코인 목록 화면 연동

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -45,6 +45,8 @@ const OverviewItem = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 33%;
+
   span:first-child {
     font-size: 10px;
     font-weight: 400;
@@ -69,11 +71,11 @@ const Tab = styled.span<{ isActive: boolean }>`
   font-size: 12px;
   font-weight: 400;
   background-color: rgba(0, 0, 0, 0.5);
-  padding: 7px 0px;
   border-radius: 10px;
   color: ${(props) =>
     props.isActive ? props.theme.accentColor : props.theme.textColor};
   a {
+    padding: 7px 0px;
     display: block;
   }
 `;
@@ -193,7 +195,9 @@ function Coin() {
                   <span>{info?.open_source ? "Yes" : "No"}</span>
                 </OverviewItem>
               </Overview>
+
               <Description>{info?.description}</Description>
+
               <Overview>
                 <OverviewItem>
                   <span>Total Suply:</span>
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
 import styled from "styled-components";
+import { fetchCoins } from "../api";
 
 const Container = styled.div`
     padding: 0px 20px;
@@ -64,20 +66,22 @@ interface CoinInferface {
 
 // a href 는 redirect 시킴, link 사용 
 function Coins() {
-    const [coins, setCoins] = useState<CoinInferface[]>([]);
-    const [loading, setLoading] = useState(true);
+    // const [coins, setCoins] = useState<CoinInferface[]>([]);
+    // const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        // https://api.coinpaprika.com/v1/coins
-        (async() => {
-            // execute immediately 
-            const response = await fetch("https://api.coinpaprika.com/v1/coins");
-            const json = await response.json();
+    // useEffect(() => {
+    //     // https://api.coinpaprika.com/v1/coins
+    //     (async() => {
+    //         // execute immediately 
+    //         const response = await fetch("https://api.coinpaprika.com/v1/coins");
+    //         const json = await response.json();
 
-            setCoins(json.slice(0, 100));
-            setLoading(false);
-        })(); 
-    }, []) // component start 시작 시에만 수행
+    //         setCoins(json.slice(0, 100));
+    //         setLoading(false);
+    //     })(); 
+    // }, []) // component start 시작 시에만 수행
+
+    const { isLoading, data } = useQuery<CoinInferface[]>("allCoins", fetchCoins)
 
     return (
         <Container>
@@ -85,11 +89,11 @@ function Coins() {
                 <Title>코인</Title>
             </Header>
             {
-                loading ? (
+                isLoading ? (
                     <Loader>"Loading"</Loader>
                 ) : <CoinsList>
                     {
-                        coins.map(coin => (
+                        data?.slice(0, 100).map(coin => (
                         <Coin key={coin.id}>
                             <Link to={{
                                     pathname: `/${coin.id}`,
