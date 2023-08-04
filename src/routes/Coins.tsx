198c092779c301e9982c3898ea4e45d5f8c6fd0d
may <soo.toance@gmail.com>
2023-08-04 13:07:13 +0900

    #5.2 Home part One - coins API 샘플 추가

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -15,7 +15,35 @@ const Title = styled.h1`
     color: ${props => props.theme.accentColor}
 `;
 
-
+const coins = [
+    {
+        id: "btc-bitcoin",
+        name: "Bitcoin",
+        symbol: "BTC",
+        rank: 1,
+        is_new: false,
+        is_active: true,
+        type: "coin",
+        },
+        {
+        id: "eth-ethereum",
+        name: "Ethereum",
+        symbol: "ETH",
+        rank: 2,
+        is_new: false,
+        is_active: true,
+        type: "coin",
+        },
+        {
+        id: "hex-hex",
+        name: "HEX",
+        symbol: "HEX",
+        rank: 3,
+        is_new: false,
+        is_active: true,
+        type: "token",
+        }
+];
 
 function Coins() {
     return (
@@ -24,7 +52,7 @@ function Coins() {
                 <Title>코인</Title>
             </Header>
             <CoinsList>
-                <Coin></Coin>
+                {coins.map(coin => <Coin key={coin.id}>{coin.name}</Coin>)}
             </CoinsList>
         </Container>
     )
