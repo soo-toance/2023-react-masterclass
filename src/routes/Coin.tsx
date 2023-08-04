0898f0a49e8a1796730b1f246dc0b5f91782e7f5
may <soo.toance@gmail.com>
2023-08-04 15:51:19 +0900

    #5.10 React Query part Two - 코인 상세 화면 React Query로 변경

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -6,7 +6,9 @@ import {
     useParams,
     useRouteMatch,
   } from "react-router-dom";
-  import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+import { fetchCoinInfo, fetchCoinTickers } from "../api";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Chart from "./Chart";
 import Price from "./Price";
@@ -144,37 +146,26 @@ interface InfoData {
   }
 
 function Coin() {
-    const [ loading, setLoading] = useState(true);
     const { coinId } = useParams<RouteParams>();
     const { state } = useLocation<RouteState>();
-    const [ info, setInfo ] = useState<InfoData>();
-    const [ priceInfo, setPriceInfo ] = useState<PriceData>();
     const priceMatch = useRouteMatch("/:coinId/price");
     const chartMatch = useRouteMatch("/:coinId/chart");
 
-    useEffect(() => {
-        // https://api.coinpaprika.com/v1/coins
-        (async() => {
-            // execute immediately 
-            const infoData = await (
-                await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
-            ).json();
-
-            const priceData = await (
-                await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
-            ).json();
-
-            setLoading(false);
-            setInfo(infoData);
-            setPriceInfo(priceData);
-        })(); 
-    }, [coinId]) // component start 시작 시에만 수행
+    const { isLoading: infoLoading, data: infoData } = useQuery<InfoData>(
+        ["info", coinId],
+        () => fetchCoinInfo(coinId)
+      );
+    const { isLoading: tickersLoading, data: tickersData } = useQuery<PriceData>(
+        ["tickers", coinId],
+        () => fetchCoinTickers(coinId)
+    );
+    const loading = infoLoading || tickersLoading;
 
     return (
         <Container>
           <Header>
             <Title>
-              {state?.name ? state.name : loading ? "Loading..." : info?.name}
+              {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
             </Title>
           </Header>
           {loading ? (
@@ -184,28 +175,28 @@ function Coin() {
               <Overview>
                 <OverviewItem>
                   <span>Rank:</span>
-                  <span>{info?.rank}</span>
+                  <span>{infoData?.rank}</span>
                 </OverviewItem>
                 <OverviewItem>
                   <span>Symbol:</span>
-                  <span>${info?.symbol}</span>
+                  <span>${infoData?.symbol}</span>
                 </OverviewItem>
                 <OverviewItem>
                   <span>Open Source:</span>
-                  <span>{info?.open_source ? "Yes" : "No"}</span>
+                  <span>{infoData?.open_source ? "Yes" : "No"}</span>
                 </OverviewItem>
               </Overview>
 
-              <Description>{info?.description}</Description>
+              <Description>{infoData?.description}</Description>
 
               <Overview>
                 <OverviewItem>
                   <span>Total Suply:</span>
-                  <span>{priceInfo?.total_supply}</span>
+                  <span>{tickersData?.total_supply}</span>
                 </OverviewItem>
                 <OverviewItem>
                   <span>Max Supply:</span>
-                  <span>{priceInfo?.max_supply}</span>
+                  <span>{tickersData?.max_supply}</span>
                 </OverviewItem>
               </Overview>
 
