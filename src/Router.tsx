30884c9fb5badda15259cfd125f1be68ce8f5f6f
may <soo.toance@gmail.com>
2023-08-05 01:32:14 +0900

    github 배포 후 url 변경에 따른 browserrouter 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,7 +5,7 @@ import Coin from "./routes/Coin";
 
 function Router() {
     return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Switch>
             <Route path="/:coinId">
                 <Coin />
