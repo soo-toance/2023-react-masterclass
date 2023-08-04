a20969186f0a9953d38133e4c9aaea8f52caea05
may <soo.toance@gmail.com>
2023-08-04 16:25:14 +0900

    #5.13 Price Chart part Two - apexcharts 와 시세 연동

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,6 +1,19 @@
 import { useParams } from "react-router";
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: string;
+  volume: number;
+  market_cap: number;
+}
 
 interface ChartProps {
   coinId: string;
@@ -8,12 +21,54 @@ interface ChartProps {
 
 function Chart({ coinId } : ChartProps) { // parameter 가져오는 방법 2 
   // const params = useParams(); // parameter 가져오는 방법 1 
-  const {isLoading, data } = useQuery(
-      ["tickers", coinId],
+  const {isLoading, data } = useQuery<IHistorical[]>(
+      [ "fetchCoinHistory", coinId ],
       () => fetchCoinHistory(coinId)
   );
-  
-  return <h1>Chart</h1>;
+
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => parseFloat(price.close)) ?? []
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            grid: { show: false },
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: { show: false },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
