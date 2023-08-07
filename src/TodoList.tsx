6b7cd973c36f15222f65528c3271daaa625223a8
may <soo.toance@gmail.com>
2023-08-07 17:31:45 +0900

    #6.8 Form Errors - 에러메시지 표기하는 기능 추가

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -32,26 +32,43 @@ import { useForm } from "react-hook-form"
 //     </div>;
 // }
 
+interface IForm {
+    toDo: string;
+}
+
 
 function ToDoList() {
-    const { register, watch, handleSubmit, formState } = useForm();
+    const { register, watch, handleSubmit, formState } = useForm<IForm>({
+        defaultValues: {
+            toDo: "@naver.com"
+        }
+    });
     // console.log(watch) // 추적 가능 
     // console.log(register("toDo")); // name, onBlur, onChange 리턴 .. 
     // console.log(formState);
 
-    const onValid = (data:any) => {
+    const onValid = (data:IForm) => {
         console.log(data);
     };
+
+    console.log(formState.errors);
 
 
     return <div>
         <form onSubmit={handleSubmit(onValid)}>
-            <input {...register("ToDo", { 
+            <input {...register("toDo", { 
                 required: 'ToDo is required', 
+                pattern: {
+                    value : /^[A-Za-z0-9._%+-]+@naver.com$/,
+                    message: "Only naver.com emails allowed"
+                },
                 minLength: {value:10, message: "your Todo is too short"} 
             })} placeholder="Write a to do" />
             <button>Add</button>
         </form>
+        <span>
+            {formState.errors?.toDo?.message}
+        </span>
     </div>;
 }
 
