384a747ab14f48c806033b6f1a22f919b7435a53
may <soo.toance@gmail.com>
2023-08-07 17:03:03 +0900

    #6.6 Forms - useState 통해 에러 메시지 추가

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -2,22 +2,31 @@ import React, { useState } from "react";
 
 function ToDoList() {
     const [ toDo, setTodo ] = useState("");
+    const [ toDoError, setToDoError ] = useState("");
+
     const onChange = (event:React.FormEvent<HTMLInputElement>) => {
         const {
             currentTarget: {value}
         } = event;
+        
+        setToDoError("");
         setTodo(value);
     };
 
     const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        console.log(toDo);
+        if (toDo.length < 10) {
+            return setToDoError("To do should be longer");
+        }
+
+        console.log('submit');
     };
 
     return <div>
         <form onSubmit={onSubmit}>
             <input onChange={onChange} placeholder="Write a to do" />
             <button>Add</button>
+            {toDoError !== "" ? toDoError : null }
         </form>
     </div>;
 }
