acab1bf17c8b16284eda2b670c31452c78105114
may <soo.toance@gmail.com>
2023-08-09 17:42:15 +0900

    #8.0 Introduction - 프로젝트 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,107 +1,26 @@
-import React, { useState } from "react"
-import styled, { createGlobalStyle } from "styled-components"
-import Router from "./Router";
-import { ReactQueryDevtools } from "react-query/devtools";
-import { ThemeProvider } from "styled-components";
-import { darkTheme, lightTheme } from './theme';
-import { themeAtom } from "./atoms";
-import { RecoilRoot, useRecoilValue } from "recoil";
-import ToDoList from "./components/TodoList";
+import styled from "styled-components";
 
-const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, menu, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed,
-figure, figcaption, footer, header, hgroup,
-main, menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  font-size: 100%;
-  font: inherit;
-  vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure,
-footer, header, hgroup, main, menu, nav, section {
-  display: block;
-}
-/* HTML5 hidden-attribute fix for newer browsers */
-*[hidden] {
-    display: none;
-}
-body {
-  line-height: 1;
-}
-menu, ol, ul {
-  list-style: none;
-}
-blockquote, q {
-  quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-  content: '';
-  content: none;
-}
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-}
-* {
-  box-sizing: border-box;
-}
-body {
-  font-family: 'Source Sans Pro', sans-serif;
-  background-color: ${(props) => props.theme.bgColor};
-  color: ${(props) => props.theme.textColor};
-}
-a {
-  text-decoration: none;
-  color: inherit;
-}
-
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-const ThemeBtn = styled.button`
-  font-size: 12px;
-  margin: 10px auto;
-`;
-
-const TopConatiner = styled.div`
-  text-align: center;
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
-
-  // const [theme, setTheme] = useState<string>();
-  // const toggleTheme = () => {
-  //   setTheme(theme === 'light' ? 'dark' : 'light')
-  // };
-  
-  const theme = useRecoilValue(themeAtom);
-
   return (
-    <>
-     <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme }>
-        <GlobalStyle />
-        {/* <RecoilRoot> */}
-        <ToDoList />
-        {/* </RecoilRoot> */}
-        <Router/>
-        <ReactQueryDevtools initialIsOpen={true} />
-      </ThemeProvider>
-    </>
+    <Wrapper>
+      <Box />
+    </Wrapper>
   );
 }
-
 export default App;
