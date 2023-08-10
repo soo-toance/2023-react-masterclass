e91d5a5c6efedc3ff346736390958d8add53ec9b
may <soo.toance@gmail.com>
2023-08-10 18:37:49 +0900

    #8.10 SVG Animation - svg 이미지 애니메이션 효과 (fill, pathLength)
    https://fontawesome.com/v5/icons/airbnb?f=brands&s=solid

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {motion, useMotionValue, useTransform, useViewportScroll } from "framer-m
 import { useEffect, useRef } from "react";
 
 const Wrapper = styled(motion.div)`
-  height: 200vh;
+  height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
@@ -26,57 +26,43 @@ const Circle = styled(motion.div)`
   border-radius: 35px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
-
-const boxVariants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: "100px" },
-  drag: { backgroundColor: "rgb(46, 204, 113)", transition: { duration: 10 } },
-};
-
-// 색상 추가 시 rgb 형식으로 넣어야 애니메이션이 진행됨 
-// backgroundColor: 'rgb(43,204,121)' 
-
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.4);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  over-flow: hidden;
+const Svg = styled.svg`
+  width: 300px;
+  height: 300px;
+  path {
+    stroke: white;
+    stroke-width: 2;
+  }
 `;
 
+const svg = {
+  start: { pathLength: 0, fill: "rgba(255, 255, 255, 0)" },
+  end: {
+    fill: "rgba(255, 255, 255, 1)",
+    pathLength: 1,
+  },
+};
 
 function App() {
-  const x = useMotionValue(0);
-
-  // motionValue는 리랜더링 하지 않기 때문에, useEffect 훅 활용해 console 찍어줘야 함 
-  
-  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
-  // useEffect(() => {
-  //   potato.onChange(() => console.log(potato.get()))}
-  //   , [x]
-  // );
-  const gradient = useTransform(
-    x,
-    [-800, 800],
-    [
-      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
-      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
-    ]
-  );
-  const { scrollYProgress } = useViewportScroll();
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
-
   return (
-    <Wrapper style={{background: gradient }}>
-        <Box
-          style={{ x , rotateZ, scale }}
-          dragSnapToOrigin
-          drag="x"
-        />
-    </Wrapper>
+    <Wrapper>
+    <Svg
+      focusable="false"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 448 512"
+    >
+      <motion.path
+        variants={svg}
+        initial="start"
+        animate="end"
+        transition={{
+          default: { duration: 5 },
+          fill: { duration: 1, delay: 3 },
+        }}
+        d="M224 373.12c-25.24-31.67-40.08-59.43-45-83.18-22.55-88 112.61-88 90.06 0-5.45 24.25-20.29 52-45 83.18zm138.15 73.23c-42.06 18.31-83.67-10.88-119.3-50.47 103.9-130.07 46.11-200-18.85-200-54.92 0-85.16 46.51-73.28 100.5 6.93 29.19 25.23 62.39 54.43 99.5-32.53 36.05-60.55 52.69-85.15 54.92-50 7.43-89.11-41.06-71.3-91.09 15.1-39.16 111.72-231.18 115.87-241.56 15.75-30.07 25.56-57.4 59.38-57.4 32.34 0 43.4 25.94 60.37 59.87 36 70.62 89.35 177.48 114.84 239.09 13.17 33.07-1.37 71.29-37.01 86.64zm47-136.12C280.27 35.93 273.13 32 224 32c-45.52 0-64.87 31.67-84.66 72.79C33.18 317.1 22.89 347.19 22 349.81-3.22 419.14 48.74 480 111.63 480c21.71 0 60.61-6.06 112.37-62.4 58.68 63.78 101.26 62.4 112.37 62.4 62.89.05 114.85-60.86 89.61-130.19.02-3.89-16.82-38.9-16.82-39.58z"
+      ></motion.path>
+    </Svg>
+  </Wrapper>
   );
 }
 export default App;
