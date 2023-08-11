e4fe71409cd067cf059f03fc54f767310a51debb
may <soo.toance@gmail.com>
2023-08-11 16:27:46 +0900

    #8.15 Final Project part One - 4개의 창 + 팝업 overlay 기능 개발

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,7 @@ const Wrapper = styled(motion.div)`
 `;
 
 const Box = styled(motion.div)`
-  width: 400px;
-  height: 400px;
+  height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   display: flex;
@@ -22,51 +21,49 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const box = {
-  entry: (back:boolean) => ({
-    x: back ? -500 : 500,
-    opacity:0,
-    scale: 1,
-  }),
-  center: {
-    x:0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 2,
-    },
-  },
-  exit:  (back:boolean) => ({
-    x: back ? 500 : -500,
-    opacity:0,
-    scale: 1,
-  }),
-};
-
-const Circle = styled(motion.div)`
-  background-color: #00a5ff;
-  height: 100px;
-  width: 100px; 
-  border-radius: 50px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div: last-child { 
+    grid-column: span 2;
+  }
 `;
 
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center; 
+  align-items: center;
+`;
 
 function App() {
   const [clicked, setClicked] = useState(false);
   const toggleClicked = () => setClicked((prev) => !prev);
 
-
   return (
     <Wrapper onClick={toggleClicked}>
-          <Box>
-            {!clicked ? <Circle layoutId="circle"/> : null }
-          </Box> 
+      <Grid>
+        <Box  layoutId="hello" />
+        <Box />
+        <Box />
+        <Box />
+      </Grid>
 
-          <Box>
-            {clicked ? <Circle layoutId="circle"/> : null }
-          </Box> 
-  </Wrapper>
+      <AnimatePresence>
+        {clicked ? <Overlay 
+          initial={{backgroundColor: "rgba(0,0,0,0)"}} 
+          animate={{backgroundColor: "rgba(0,0,0,0.5)"}} 
+          exit={{backgroundColor: "rgba(0,0,0,0)"}}
+          >
+            <Box style={{width:400,height:200}} layoutId="hello"/>
+          </Overlay> : null }
+      </AnimatePresence>
+    </Wrapper>
   );
 }
 export default App;
